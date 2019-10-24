83fbd1deb2c62f5375a63aa5c604dbb01a636b78
Adrian Oviedo <adrian.oviedo.1992@gmail.com>
2019-10-24 14:47:04 +0200

    mocking some data

---
+++ src/mockData.js
@@ -0,0 +1,25 @@
+export const ROLES = [
+  {
+    id: "ogdb33RJ0el2A",
+    name: "Role 1",
+    permissions: ['ACCOUNT:READ_ACCESS', 'STORE:READ', 'PROJECT:WRITE']
+  },
+  {
+    id: "Z1z1vNCvRy7hF",
+    name: "Role 2",
+    permissions: ['ENTITY1:PERM 1_PERM 2_PERM 3', 'ENTITY2:PERM 1_PERM 2_PERM 3']
+  },
+  {
+    id: "LI6rMPmykXbfd",
+    name: "Role 3",
+    permissions: ['ACCOUNT:READ_ACCESS', 'ENTITY2:PERM 1_PERM 2_PERM 3']
+  }
+];
+
+export const PERMISSIONS = [
+  'ACCOUNT:READ_ACCESS',
+  'STORE:READ',
+  'PROJECT:WRITE',
+  'ENTITY1:PERM 1_PERM 2_PERM 3',
+  'ENTITY2:PERM 1_PERM 2_PERM 3'
+];
