16e155d2f7da83ca49686bae7f566bbacdfb12dc
Adrian Oviedo <adrian.oviedo.1992@gmail.com>
2019-10-26 12:02:12 +0200

    fixing mocking data ids

--- src/mockData.js
+++ src/mockData.js
@@ -15,108 +15,108 @@ export const ROLES = [
     permissions: ['ACCOUNT:READ_ACCESS', 'ENTITY2:PERM 1_PERM 2_PERM 3']
   },
   {
-    id: "ogdb33RJ0el2A",
-    name: "Role 1",
+    id: "ogdb33RJ0el2A1",
+    name: "Role 11",
     permissions: ['ACCOUNT:READ_ACCESS', 'STORE:READ', 'PROJECT:WRITE']
   },
   {
-    id: "Z1z1vNCvRy7hF",
-    name: "Role 2",
+    id: "Z1z1vNCvRy7hF2",
+    name: "Role 22",
     permissions: ['ENTITY1:PERM 1_PERM 2_PERM 3', 'ENTITY2:PERM 1_PERM 2_PERM 3']
   },
   {
-    id: "LI6rMPmykXbfd",
-    name: "Role 3",
+    id: "LI6rMPmykXbfd3",
+    name: "Role 33",
     permissions: ['ACCOUNT:READ_ACCESS', 'ENTITY2:PERM 1_PERM 2_PERM 3']
   },
   {
-    id: "ogdb33RJ0el2A",
-    name: "Role 1",
+    id: "ogdb33RJ0el2A4",
+    name: "Role 14",
     permissions: ['ACCOUNT:READ_ACCESS', 'STORE:READ', 'PROJECT:WRITE']
   },
   {
-    id: "Z1z1vNCvRy7hF",
-    name: "Role 2",
+    id: "Z1z1vNCvRy7hF5",
+    name: "Role 25",
     permissions: ['ENTITY1:PERM 1_PERM 2_PERM 3', 'ENTITY2:PERM 1_PERM 2_PERM 3']
   },
   {
-    id: "LI6rMPmykXbfd",
-    name: "Role 3",
+    id: "LI6rMPmykXbfd6",
+    name: "Role 36",
     permissions: ['ACCOUNT:READ_ACCESS', 'ENTITY2:PERM 1_PERM 2_PERM 3']
   },
   {
-    id: "ogdb33RJ0el2A",
-    name: "Role 1",
+    id: "ogdb33RJ0el2A7",
+    name: "Role 17",
     permissions: ['ACCOUNT:READ_ACCESS', 'STORE:READ', 'PROJECT:WRITE']
   },
   {
-    id: "Z1z1vNCvRy7hF",
-    name: "Role 2",
+    id: "Z1z1vNCvRy7hF8",
+    name: "Role 28",
     permissions: ['ENTITY1:PERM 1_PERM 2_PERM 3', 'ENTITY2:PERM 1_PERM 2_PERM 3']
   },
   {
-    id: "LI6rMPmykXbfd",
-    name: "Role 3",
+    id: "LI6rMPmykXbfd9",
+    name: "Role 39",
     permissions: ['ACCOUNT:READ_ACCESS', 'ENTITY2:PERM 1_PERM 2_PERM 3']
   },
   {
-    id: "ogdb33RJ0el2A",
-    name: "Role 1",
+    id: "ogdb33RJ0el2A0",
+    name: "Role 10",
     permissions: ['ACCOUNT:READ_ACCESS', 'STORE:READ', 'PROJECT:WRITE']
   },
   {
-    id: "Z1z1vNCvRy7hF",
-    name: "Role 2",
+    id: "Z1z1vNCv9Ry7hF",
+    name: "Role 92",
     permissions: ['ENTITY1:PERM 1_PERM 2_PERM 3', 'ENTITY2:PERM 1_PERM 2_PERM 3']
   },
   {
-    id: "LI6rMPmykXbfd",
-    name: "Role 3",
+    id: "LI6rMPmy8kXbfd",
+    name: "Role 83",
     permissions: ['ACCOUNT:READ_ACCESS', 'ENTITY2:PERM 1_PERM 2_PERM 3']
   },
   {
-    id: "ogdb33RJ0el2A",
-    name: "Role 1",
+    id: "ogdb33RJ70el2A",
+    name: "Role 71",
     permissions: ['ACCOUNT:READ_ACCESS', 'STORE:READ', 'PROJECT:WRITE']
   },
   {
-    id: "Z1z1vNCvRy7hF",
-    name: "Role 2",
+    id: "Z1z1vN7CvRy7hF",
+    name: "Role 72",
     permissions: ['ENTITY1:PERM 1_PERM 2_PERM 3', 'ENTITY2:PERM 1_PERM 2_PERM 3']
   },
   {
-    id: "LI6rMPmykXbfd",
-    name: "Role 3",
+    id: "LI6rM6PmykXbfd",
+    name: "Role 63",
     permissions: ['ACCOUNT:READ_ACCESS', 'ENTITY2:PERM 1_PERM 2_PERM 3']
   },
   {
-    id: "ogdb33RJ0el2A",
-    name: "Role 1",
+    id: "ogdb33RJ05el2A",
+    name: "Role 51",
     permissions: ['ACCOUNT:READ_ACCESS', 'STORE:READ', 'PROJECT:WRITE']
   },
   {
-    id: "Z1z1vNCvRy7hF",
-    name: "Role 2",
+    id: "Z1z1vNC4vRy7hF",
+    name: "Role 42",
     permissions: ['ENTITY1:PERM 1_PERM 2_PERM 3', 'ENTITY2:PERM 1_PERM 2_PERM 3']
   },
   {
-    id: "LI6rMPmykXbfd",
-    name: "Role 3",
+    id: "LI6rMPm4ykXbfd",
+    name: "Role 43",
     permissions: ['ACCOUNT:READ_ACCESS', 'ENTITY2:PERM 1_PERM 2_PERM 3']
   },
   {
-    id: "ogdb33RJ0el2A",
-    name: "Role 1",
+    id: "ogdb33RJ30el2A",
+    name: "Role 31",
     permissions: ['ACCOUNT:READ_ACCESS', 'STORE:READ', 'PROJECT:WRITE']
   },
   {
-    id: "Z1z1vNCvRy7hF",
-    name: "Role 2",
+    id: "Z1z1vNC2vRy7hF",
+    name: "Role 222",
     permissions: ['ENTITY1:PERM 1_PERM 2_PERM 3', 'ENTITY2:PERM 1_PERM 2_PERM 3']
   },
   {
-    id: "LI6rMPmykXbfd",
-    name: "Role 3",
+    id: "LI6rMPmy1kXbfd",
+    name: "Role 13",
     permissions: ['ACCOUNT:READ_ACCESS', 'ENTITY2:PERM 1_PERM 2_PERM 3']
   },
 ];
